ef430e26bb1d9e1fc2dfb36cb358a2e62b6daf4d
Elena <elenalapalmera17@gmail.com>
2026-03-19 11:13:51 +0100

    Añadir comentarios e intento de arreglo para mostrar datos

--- src/main/java/daw/PanelBotones.java
+++ src/main/java/daw/PanelBotones.java
@@ -14,11 +14,12 @@ public class PanelBotones extends JPanel {
 
     private void initComponents() {
 
-        // Creación de los botones
+        // Creación de los botones del 0 al 9
         for (int i = 0; i < 10; i++) {
             grupoBotones[i] = new JButton(Integer.toString(i));
         }
 
+        // Añadir botones con los operadores a las últimas posiciones de la lista
         grupoBotones[10] = new JButton("+");
         grupoBotones[11] = new JButton("-");
         grupoBotones[12] = new JButton("*");
@@ -26,7 +27,7 @@ public class PanelBotones extends JPanel {
         grupoBotones[14] = new JButton("=");
         grupoBotones[15] = new JButton("C");
 
-        // Layout tipo cuadrícula
+        // Layout tipo cuadrícula 
         this.setLayout(new GridLayout(4, 4));
 
         for (JButton boton : grupoBotones) {
--- src/main/java/daw/PanelPrincipal.java
+++ src/main/java/daw/PanelPrincipal.java
@@ -9,26 +9,32 @@ import javax.swing.JTextArea;
 
 public class PanelPrincipal extends JPanel implements ActionListener {
 
+    // Creo el objeto que me crea los botones
     private PanelBotones botonera;
     private JTextArea areaTexto;
 
     private double operando1 = 0;
     private String operador = "";
 
+    // Crear la ventana principal
     public PanelPrincipal() {
         initComponents();
     }
 
+    // Inicializar los componentes que tiene la ventana principal
     private void initComponents() {
 
+        // Crear los botones
         botonera = new PanelBotones();
 
+        // Crear un área de texto con unas dimensiones y un color de fondo
         areaTexto = new JTextArea(3, 20);
         areaTexto.setEditable(false);
         areaTexto.setBackground(Color.WHITE);
 
         this.setLayout(new BorderLayout());
 
+        // Diferenciar la pantalla donde aparecen los botones (la parte sur) con la parte del texto (la parte norte)
         this.add(areaTexto, BorderLayout.NORTH);
         this.add(botonera, BorderLayout.SOUTH);
 
@@ -38,12 +44,16 @@ public class PanelPrincipal extends JPanel implements ActionListener {
         }
     }
 
+    // Método sobreescrito al implementar el ActionListener
     @Override
     public void actionPerformed(ActionEvent ae) {
 
+        // Recogo en un botón qué botón concretamente he pulsado en la calculadora
         JButton boton = (JButton) ae.getSource();
+        // Guardo en una variable el contenido del botón que he pulsado
         String texto = boton.getText();
 
+        // Si el contenido del botón coincide con un número dentro del rango de 1 al 9, lo muestro en el área de texto
         if (texto.matches("[0-9]")) {
             areaTexto.append(texto);
         }
@@ -51,7 +61,7 @@ public class PanelPrincipal extends JPanel implements ActionListener {
         else if (texto.matches("[+\\-*/]")) {
             operando1 = Double.parseDouble(areaTexto.getText());
             operador = texto;
-            areaTexto.setText("");
+            areaTexto.append(" " + operador + " ");
         }
 
         else if (texto.equals("=")) {
@@ -74,7 +84,7 @@ public class PanelPrincipal extends JPanel implements ActionListener {
                     break;
             }
 
-            areaTexto.setText(Double.toString(resultado));
+            areaTexto.append(resultado + " ");
         }
 
         else if (texto.equals("C")) {
